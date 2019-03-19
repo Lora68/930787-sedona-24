4151f86708605ddd74cdfe02a61e23e5abecf093
Lora68 <46938250+Lora68@users.noreply.github.com>
2019-03-19 19:06:50 +0300

    Исправления защита 1

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 var search_wrap = document.querySelector(".search_wrap");
-var  button_search = document.querySelector(".button_search");
+var button_search = document.querySelector(".button_search");
+var date_in = document.getElementById("search_data_check-in");
+var date_out = document.getElementById("search_data_check-out");
+var form = document.querySelector(".button-submit");
 
 button_search.addEventListener("click",function(evt){
   evt.preventDefault();
   search_wrap.classList.toggle("show");
+  search_wrap.classList.remove("form_error");
 });
